61e4959e508af5c09bbdca75c5f41d0be074fb37
Omar Mohamed Yehia <93586279+omaryehia012@users.noreply.github.com>
2023-10-01 19:28:53 +0300

    Update streamlitui.py

--- streamlitui.py
+++ streamlitui.py
@@ -49,7 +49,11 @@ def is_openai_api_key_set() -> bool:
 def main():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
-        st.session_state["pdfquery"] = PDFQuery(OPENAI_API_KEY)
+        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
+        if is_openai_api_key_set():
+            st.session_state["pdfquery"] = PDFQuery(st.session_state["OPENAI_API_KEY"])
+        else:
+            st.session_state["pdfquery"] = None
 
     st.header("ChatPDF")
 
